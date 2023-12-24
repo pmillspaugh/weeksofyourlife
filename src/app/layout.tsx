66f8eb2a6ac34f502a489132fb4562dac956cb2a
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2023-12-23 22:15:55 -0500

    Add Vercel analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Lora, Fira_Code } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+
 import "./globals.css";
 
 const victorMono = Fira_Code({ subsets: ["latin"], variable: "--font-mono" });
@@ -20,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${victorMono.variable} ${lora.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
