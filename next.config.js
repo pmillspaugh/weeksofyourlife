b1e8360b4ed387069790f141f01f3a615044f9a1
Pete Millspaugh <73900714+pmillspaugh@users.noreply.github.com>
2024-09-11 19:25:44 -0400

    Configure onlook; bump Next.js version (#13)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    swcPlugins: [["@onlook/nextjs", { root: path.resolve(".") }]],
+  },
+};
+
+module.exports = nextConfig;
